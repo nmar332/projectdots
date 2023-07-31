d263fc851c5dbabf38933015c5c7c171cf45d564
Nick Marriott <nick.marriott@rea-group.com>
2023-07-31 12:18:15 +1000

    loop

--- sketch.js
+++ sketch.js
@@ -8,84 +8,87 @@ let border;
 
 let angle = 0;
 
-let imgUrl = "temp.jpeg";
+let imgUrl = "NICK.png";
 let img;
 let particles = [];  
+let initParticles = [];
 
-const PARTICLE_SIZE = 10; //how big the dots are
-const RESOLUTION = 40; //the total space between each particle
+const PARTICLE_SIZE = 3 //how big the dots are
+const RESOLUTION = 8; //the total space between each particle, higher = less
 
 const noiseScale =0.01/2;
 const num = 100;
-
-
-// Plan
-// clear template **DONE**
-// duplicate code to the point of image to dots **DONE**
-// get the dots waving
-// maybe add some distorion?
-// swap the setup to take text input?
-
-// Do we want to convert the key points of an image into 
-
 
 function preload() {
   img = loadImage(imgUrl);
 }
 
 function setup() {
-  // updateContainer();
   createCanvas(windowWidth, windowHeight);
-  smooth();
-  // canvas.parent("#sketchContainer");
   spawnParticles();
-  
 }
 
 function draw() {
-  // background(200);
-  // image(img, 0, 0, width, height);
-  // filter(GRAY);
-
+  background(255);
+  noStroke();
   particles.forEach((particle ) => {
     particle.draw();
-    let n = noise(p.x * noiseScale, p.y * noiseScale, frameCount * noiseScale * noiseScale);
-    let a = TAU * n;
-    p.x += cos(a);
-    p.y += sin(a);
-    if(!onScreen(p)) {
-      p.x = random(width);
-      p.y = random(height);
-    }
-  })
+    particle.update();
+    })
 
-
-  // for(let i = 0; i < num; i ++) {
-  //   let p = particles[i];
-  //   console.log(p)
-  //   point(p.x, p.y);
-  //   stroke(p.color);
   //   let n = noise(p.x * noiseScale, p.y * noiseScale, frameCount * noiseScale * noiseScale);
-  //   let a = TAU * n;
-  //   p.x += cos(a);
-  //   p.y += sin(a);
-  //   if(!onScreen(p)) {
-  //     p.x = random(width);
-  //     p.y = random(height);
-  //   }
-  // }
+  //   let a = TAU * n; // TAU is 2x pi
+  //   p.x += sin(n);// 0 keeps it still along the axis.
+  //   p.y += cos(n); // cos for x and sin for y. USE CONSTRAIN
+  //   console.log(constrain(p.x+a, initParticles[count].x-20, initParticles[count].x+20))
+
+  // //   if(p.x < initParticles[count].x-20 || p.x > initParticles[count].x+20){
+  // //   // p.x = constrain(p.x+a, initParticles[count].x-20, initParticles[count].x+20);
+  // //   p.x = initParticles[count].x;
+  // //   //p.y = constrain(p.y+a, initParticles[count].y-20, initParticles[count].y+20);
+  // // }else if (p.x == initParticles[count].x){
+  // //   p.x += sin(n);// 0 keeps it still along the axis.
+  // // }
+  // // //While less than 10 keep moving forward, when it hits 10 then reverse direction until back to start. 
+    
+  //   ellipse(p.x, p.y, PARTICLE_SIZE);
+
+  // //RESET animation once gone too far
+  // // if(abs(p.x) > initParticles[count].x +5 || abs(p.x) < initParticles[count].x -5 &&
+  // //    abs(p.y) > initParticles[count].y +5 || abs(p.y) < initParticles[count].y -5){
+  // //     p.x += acos(a)
+  // //     p.y += asin(a)
+  // // } 
+
+
+  // // Single point is slowly wrapped. can we take its inital position and calculate distance then reset?
+  //   // if(!onScreen(p)) {
+  //   //   p.x = constraintXLow;
+  //   //   p.y = constraintYLow;
+  //   // }
+  //   count += 1;
+  //   })
 }
 
 function spawnParticles() {
+  let rows = 15;
+  let rowSize = height/rows;
+  let loc = 100;
+  
+  // FOR IMAGE ONLY
   for(i = 0; i < width; i+=RESOLUTION)  {
     for(j = 0; j < height; j+=RESOLUTION) {
       let x = i / width * img.width;
       let y = j / height * img.height;
+      
 
       const color = img.get(x, y);
-      particles.push(new Particle(i, j, color));
+      particles.push(new Particle(i, j, color,rowSize/2, i*loc+j*loc));
+      //initParticles.push(new Particle(i, j, color));
     }
   }
+
+  //FOR TEXT ONLY
 }
 
 function mouseReleased() {
@@ -97,19 +100,22 @@ function onScreen(v) {
 }
 
 class Particle {
-  constructor(x, y, color) {
-    this.x = x;
-    this.y = y;
+  constructor(x, y, color, r, angle) {
+    this.x0 = x;
+    this.y0 = y;
     this.color = color;
+    this.r = r;
+    this.angle = angle; 
   }
 
-  update() {
-
+  update(){
+    this.x = this.r*cos(this.angle);
+    this.y = this.r*sin(this.angle);
+    this.angle += 0.015;  //speed
   }
 
   draw() {
     fill(this.color);
-    noStroke();
-    ellipse(this.x, this.y, PARTICLE_SIZE);
+    ellipse(this.x0+this.x, this.y0+this.y, PARTICLE_SIZE);
   }
 }
